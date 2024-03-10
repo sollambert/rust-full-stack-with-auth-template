560cbe9132895f4cf61518541840e78efe061256
Solomon <sol.lambert@gmail.com>
2024-03-10 12:36:10 -0500

    add sql migrations

---
+++ migrations/20231214145144_InitialMigration.up.sql
@@ -0,0 +1,8 @@
+-- Add migration script here
+CREATE TABLE "users" (
+    id SERIAL PRIMARY KEY UNIQUE,
+    uuid VARCHAR(36) UNIQUE,
+    username VARCHAR(24) UNIQUE,
+    pass VARCHAR(60),
+    email VARCHAR(254) UNIQUE
+);
