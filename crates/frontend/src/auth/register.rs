ad04472a6de53ac26d339aa5b4fa0d63ca0ef05e
Solomon <sol.lambert@gmail.com>
2024-03-10 12:36:58 -0500

    prep for login and register components

---
+++ crates/frontend/src/auth/register.rs
@@ -0,0 +1,11 @@
+use tauri_sys::tauri::invoke;
+use yew::prelude::*;
+use yew_hooks::prelude::*;
+
+#[function_component(Register)]
+pub fn register() -> Html {
+    html! {
+        <>
+        </>
+    }
+}
