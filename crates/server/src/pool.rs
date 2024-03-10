5e25732607abef02354a63c5485c5f3ab48e75ee
Solomon <sol.lambert@gmail.com>
2024-03-09 19:00:10 -0600

    pg pool init error handling and env variable loading

--- crates/server/src/pool.rs
+++ crates/server/src/pool.rs
@@ -6,13 +6,25 @@ use once_cell::sync::OnceCell;
 static POOL: OnceCell<PgPool> = OnceCell::new();
 
 // function for initializing the POOL singleton
-pub async fn create_pool() -> Result<(), sqlx::Error> {
-    let pool: PgPool = PgPoolOptions::new()
+pub async fn create_pool() {
+
+    let database_url = match env::var("DATABASE_URL") {
+        Ok(url) => url,
+        Err(error) => {
+            panic!("Error getting DATABASE_URL: {}", error);
+        }
+    };
+
+    let pool = match PgPoolOptions::new()
         .max_connections(100)
         .idle_timeout(Some(Duration::from_millis(1000)))
-        .connect(&env::var("DATABASE_URL").unwrap()).await?;
+        .connect(&database_url).await {
+            Ok(pool) => pool,
+            Err(error) => {
+                panic!("Could not create pool: {}", error);
+            }
+        };
     POOL.set(pool).unwrap();
-    Ok(())
 }
 
 // getter for accessing global POOL singleton in other modules
