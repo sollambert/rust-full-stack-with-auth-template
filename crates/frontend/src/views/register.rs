63a6882cf29ddaa8c3d8500e03425447e386033a
Solomon <sol.lambert@gmail.com>
2024-03-17 20:08:24 -0500

    use effect for redirect from login and register page if signed in

--- crates/frontend/src/views/register.rs
+++ crates/frontend/src/views/register.rs
@@ -1,16 +1,29 @@
 use yew::prelude::*;
 use yew_router::history::History;
 use yew_router::history::HashHistory;
+use yewdux::functional::use_store;
 
+use crate::app::UserState;
 use crate::components::auth::registerform::RegisterForm;
 
 #[function_component(Register)]
 pub fn login() -> Html {
+    let (user_state, _user_dispatch) = use_store::<UserState>();
+
+    use_effect(move || {
+        if user_state.user_info.uuid != String::new() {
+            HashHistory::new().push("/")
+        }
+    });
 
     html! {
         <main class="flex flex-col items-center h-100">
             <RegisterForm />
-            <a class="cursor-pointer text-blue-700 underline" onclick={move |_| {HashHistory::new().push("/login")}}>{"Already have an account?"}</a>
+            <a href="javascript::;" class="cursor-pointer text-blue-700 underline
+                    focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
+                    onclick={move |_| {HashHistory::new().push("/login")}}>
+                {"Already have an account?"}
+            </a>
         </main>
     }
 }
