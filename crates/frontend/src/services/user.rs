7ca9495a8900523c2f717e7c45c3a922b6374c39
Solomon Lambert <sol.lambert@gmail.com>
2024-03-26 10:55:25 -0500

    refactor user services on frontend

--- crates/frontend/src/services/user.rs
+++ crates/frontend/src/services/user.rs
@@ -5,69 +5,83 @@ use types::user::UserInfo;
 use super::{get_http_auth_client, get_http_client, AuthStorage};
 
 pub async fn get_user_info() -> UserInfo {
-    let auth_token = AuthStorage::get_requester_token();
-    if let Err(_) = auth_token {
+    // Attempt to get auth requester token from storage
+    let auth_token_result = AuthStorage::get_requester_token();
+    if let Err(_) = auth_token_result {
         return UserInfo::new();
     }
-    let auth_token = auth_token.unwrap();
-    let mut header_map = HeaderMap::new();
-    header_map.insert(AUTHORIZATION, HeaderValue::from_str(auth_token.to_string().as_str()).unwrap());
-    let request_builder = get_http_client().request(Method::GET, Url::parse("http://localhost:3001/user/info").unwrap())
-        .headers(header_map);
-    let response = request_builder.send().await;
-    match response {
-        Ok(data) => {
-            let status = data.status();
-            if status.is_success() {
-                match data.json::<UserInfo>().await {
-                    Ok(auth_body) => {
-                        auth_body
-                    },
-                    Err(error) => {
-                        error!("Error parsing body: {}", error.to_string());
-                        UserInfo::new()
-                    }
-                }
-            } else {
-                UserInfo::new()
-            }
-        },
-        Err(error) => {
-            error!("Error with request: {}", error.to_string());
-            UserInfo::new()
-        }
+    let auth_token = auth_token_result.unwrap();
+
+    // Build auth header from token
+    let auth_header_result = HeaderValue::from_str(auth_token.to_string().as_str());
+    if let Err(error) = auth_header_result {
+        error!("Could not create header from token: {}", error.to_string());
+        return UserInfo::new();
     }
+    let auth_header = auth_header_result.unwrap();
+
+    // Build request using header map with auth header
+    let mut header_map = HeaderMap::new();
+    header_map.insert(AUTHORIZATION, auth_header);
+    let request_builder = get_http_client()
+        .request(Method::GET, Url::parse("http://localhost:3001/user/info").unwrap())
+        .headers(header_map);
+
+    // Request user info from server
+    let request_result = request_builder.send().await;
+    if let Err(error) = request_result {
+        error!("Error with request: {}", error.to_string());
+        return UserInfo::new();
+    }
+    let response = request_result.unwrap();
+    
+    // Parse response body as JSON
+    let json_result = response.json::<UserInfo>().await;
+    if let Err(error) = json_result {
+        error!("Failed to parse response body as json: {}", error.to_string());
+        return UserInfo::new();
+    }
+
+    // Return UserInfo
+    let data = json_result.unwrap();
+    return data;
 }
 
 pub async fn get_all_users() -> Result<(StatusCode, Vec<UserInfo>), StatusCode> {
-    let response = get_http_auth_client().get("http://localhost:3001/user/all").send().await;
-    match response {
-        Ok(data) => {
-            let status = data.status().clone();
-            match data.json::<Vec<UserInfo>>().await {
-                Ok(users) => {
-                    Ok((status, users))
-                },
-                Err(_error) => {
-                    Err(status)
-                }
-            }
-        },
-        Err(error) => {
-            error!("Error with request: {}", error.to_string());
-            Err(error.status().unwrap())
-        }
+    // Request all users from server
+    let request_result = get_http_auth_client().get("http://localhost:3001/user/all").send().await;
+    if let Err(error) = request_result {
+        error!("Error with request: {}", error.to_string());
+        return Err(error.status().unwrap());
     }
+
+    // Unwrap request and extract status as owned value
+    let response = request_result.unwrap();
+    let status = response.status();
+
+    // Parse body as JSON
+    let json_result = response.json::<Vec<UserInfo>>().await;
+    if let Err(error) = json_result {
+        error!("Failed to parse response body as json: {}", error.to_string());
+        return Err(error.status().unwrap());
+    }
+
+    // Return vec of users
+    let users = json_result.unwrap();
+    Ok((status, users))
 }
 
 pub async fn delete_user(uuid: String) -> Result<StatusCode, StatusCode> {
-    let response = get_http_auth_client().delete("http://localhost:3001/user").body(uuid).send().await;
-    match response {
-        Ok(response) => {
-            Ok(response.status())
-        },
-        Err(error) => {
-            Err(error.status().unwrap())
-        }
+    // Request to delete user with uuid
+    let request_result = get_http_auth_client().delete("http://localhost:3001/user").body(uuid).send().await;
+    if let Err(error) = request_result {
+        error!("Error with request: {}", error.to_string());
+        return Err(error.status().unwrap());
     }
+
+    // Unwrap request and extract status as owned value
+    let response = request_result.unwrap();
+
+    // Return status of response
+    Ok(response.status())
 }
