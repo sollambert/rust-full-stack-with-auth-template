f30527f4e8f36cf147010ed5f25d463fdd6c18f1
Solomon Lambert <sol.lambert@gmail.com>
2024-03-26 10:05:40 -0500

    use yew Link component instead of div with onclick/onkeyevent for links between login/register pages

--- crates/frontend/src/views/login.rs
+++ crates/frontend/src/views/login.rs
@@ -1,9 +1,10 @@
 use yew::prelude::*;
+use yewdux::functional::use_store;
 use yew_router::history::History;
 use yew_router::history::HashHistory;
-use yewdux::functional::use_store;
+use yew_router::prelude::*;
 
-use crate::app::UserState;
+use crate::app::{UserState, AppRoute};
 use crate::components::auth::login_form::LoginForm;
 
 #[function_component(Login)]
@@ -19,12 +20,12 @@ pub fn login() -> Html {
     html! {
         <div class="col-span-12 row-span-24 flex flex-col justify-center items-center h-full">
             <LoginForm />
-            <div tabindex={0} class="cursor-pointer text-blue-600 dark:text-blue-400 underline
-                    focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-3"
-                    onclick={move |_| {HashHistory::new().push("/register")}}
-                    onkeypress={move |e: KeyboardEvent| {if e.key() == "Enter" { HashHistory::new().push("/register")}}}>
-                {"Need an account?"}
-            </div>
+            <Link<AppRoute> to={AppRoute::Register}>
+                <div class="cursor-pointer text-blue-600 dark:text-blue-400 underline
+                        focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-3">
+                    {"Need an account?"}
+                </div>
+            </Link<AppRoute>>
         </div>
     }
 }
--- crates/frontend/src/views/register.rs
+++ crates/frontend/src/views/register.rs
@@ -1,9 +1,10 @@
 use yew::prelude::*;
+use yewdux::functional::use_store;
 use yew_router::history::History;
 use yew_router::history::HashHistory;
-use yewdux::functional::use_store;
+use yew_router::prelude::*;
 
-use crate::app::UserState;
+use crate::app::{UserState, AppRoute};
 use crate::components::auth::register_form::RegisterForm;
 
 #[function_component(Register)]
@@ -19,12 +20,12 @@ pub fn login() -> Html {
     html! {
         <div class="col-span-12 row-span-24 flex flex-col justify-center items-center h-full">
             <RegisterForm />
-            <div tabindex={0} class="cursor-pointer text-blue-600 dark:text-blue-400 underline
-                    focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-3"
-                    onclick={move |_| {HashHistory::new().push("/login")}}
-                    onkeypress={move |e: KeyboardEvent| {if e.key() == "Enter" { HashHistory::new().push("/login")}}}>
-                {"Already have an account?"}
-            </div>
+            <Link<AppRoute> to={AppRoute::Login}>
+                <div class="cursor-pointer text-blue-600 dark:text-blue-400 underline
+                        focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-3">
+                    {"Already have an account?"}
+                </div>
+            </Link<AppRoute>>
         </div>
     }
 }
