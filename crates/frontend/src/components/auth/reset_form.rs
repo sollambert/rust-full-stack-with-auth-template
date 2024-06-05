7d5ec8d7b82a09f81df6cb442d290b524f84e3ad
sollambert <sol.lambert@gmail.com>
2024-06-05 16:33:02 -0500

    implement password confirmation on reset form

--- crates/frontend/src/components/auth/reset_form.rs
+++ crates/frontend/src/components/auth/reset_form.rs
@@ -1,6 +1,6 @@
 use email_address::EmailAddress;
 use serde::Deserialize;
-use types::user::{ResetUser, UserInfo};
+use types::{auth::AuthErrorType, user::{ResetUser, UserInfo}};
 use web_sys::HtmlInputElement;
 use yew::{function_component, html, use_state, Callback, Html, InputEvent, SubmitEvent, TargetCast, UseStateHandle};
 use yew_hooks::use_async;
@@ -17,7 +17,7 @@ struct QueryParams {
 }
 
 #[function_component(ResetForm)]
-pub fn register_form() -> Html {
+pub fn reset_form() -> Html {
     let location = use_location().unwrap();
     let query_params = location.query::<QueryParams>().unwrap();
     let initial_user = ResetUser {
@@ -27,22 +27,38 @@ pub fn register_form() -> Html {
     let (_user_state, user_dispatch) = use_store::<StoredUserInfo>();
     let error_state = use_state(|| None::<AuthError>);
     let reset_user = use_state(|| initial_user.clone());
+    let confirm_pass = use_state(|| String::new());
 
     let oninput = |key, error_state: &UseStateHandle<Option<AuthError>>| {
         let error_state = error_state.clone();
-        let register_user = reset_user.clone();
+        let reset_user = reset_user.clone();
         Callback::from(move |e: InputEvent| {
             let error_state = error_state.clone();
             if let Some(_) = *error_state {
                 error_state.set(None);
             }
             let input: HtmlInputElement = e.target_unchecked_into();
-            match register_user.update_field(key, input.value()) {
-                Ok(new_register_user) => {
-                    register_user.set(new_register_user);
+            match reset_user.update_field(key, input.value()) {
+                Ok(new_reset_user) => {
+                    reset_user.set(new_reset_user);
                 }, Err(error) => {error!(error)}
             };
         })
+    };
+
+    let on_confirm_input = |error_state: &UseStateHandle<Option<AuthError>>| {
+        let error_state = error_state.clone();
+        let reset_user = reset_user.clone();
+        let confirm_pass = confirm_pass.clone();
+        Callback::from(move |e: InputEvent| {
+            let confirm_pass_value = e.target_unchecked_into::<HtmlInputElement>().value();
+            if confirm_pass_value != reset_user.pass {
+                error_state.set(Some(AuthError::from_error_type(AuthErrorType::PasswordDoesNotMatch)));
+            } else {
+                error_state.set(None);
+            }
+            confirm_pass.set(confirm_pass_value);
+        }) 
     };
 
     let handle_reset = {
@@ -50,6 +66,12 @@ pub fn register_form() -> Html {
         let error_state = error_state.clone();
         use_async(async move {
             let response = services::auth::reset_user((*reset_user).clone(), query_params.key).await;
+            if let Some(error) = (*error_state).to_owned() {
+                match error.body().error_type {
+                    AuthErrorType::PasswordDoesNotMatch => return Err(error),
+                    _ => {()}
+                }
+            }
             match response {
                 Ok(status) => {
                     user_dispatch.set(StoredUserInfo {user_info: UserInfo::default()});
@@ -82,11 +104,14 @@ pub fn register_form() -> Html {
     };
 
     html! {
-        <form class="flex flex-col w-64 space-y-2" onsubmit={reset_onsubmit}>
+        <form class="flex flex-col w-64 space-y-2 text-center
+            text-slate-800 dark:text-slate-100" onsubmit={reset_onsubmit}>
+            <p>{"Enter your new password"}</p>
             if let Some(error) = (*error_state).to_owned() {
                 <ErrorMessage message={error.body().message} />
             }
             <Input input_type="password" placeholder="Password" oninput={oninput("pass", &error_state)} value={reset_user.pass.to_owned()} />
+            <Input input_type="password" placeholder="Confirm password" oninput={on_confirm_input(&error_state)} value={(*confirm_pass).to_owned()} />
             <Button onclick={reset_onclick} label="Confirm" />
         </form>
     }
